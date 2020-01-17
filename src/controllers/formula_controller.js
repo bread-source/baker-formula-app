21a753b0bd749a3493d9689ac2c0bc0a28c3d0a6
Bread Maker <breadmaker@example.com>
2020-01-17 15:42:59 -0500

    Make base the final dough weight, not flour weight

--- src/controllers/formula_controller.js
+++ src/controllers/formula_controller.js
@@ -4,95 +4,89 @@ export default class extends Controller {
 
   static get targets() {
     return [
-      "flourWeight",
       "ingredientTable",
       "ingredientPercent",
       "ingredientWeight",
       "totalPercent",
-      "totalWeight"
+      "totalDoughWeight"
     ]
   }
 
   connect() {
-    this.flourWeightTarget.value = 500
-    this.updateWeights()
+    this.totalDoughWeight = 500
+    this.updateIngredientWeights()
+    this.addIngredient() // Temp while I flesh out on-connect behavior
   }
 
   deleteIngredient(event) {
     const ingredientIndex = event.target.parentElement.parentElement.rowIndex
     this.ingredientTableTarget.deleteRow(ingredientIndex)
+    this.updateIngredientWeights()
   }
 
   addIngredient() {
     let newIngredientRow = this.ingredientTableTarget.insertRow()
     newIngredientRow.innerHTML = `
       <td><input data-target="formula.ingredientName" type="string"></td>
-      <td><input data-target="formula.ingredientPercent" data-action="formula#updateWeights" type="number" step="0.01"></td>
-      <td><input data-target="formula.ingredientWeight" data-action="formula#calculateFlourWeight" type="number" step="0.01"></td>
+      <td><input data-target="formula.ingredientPercent" data-action="formula#updateIngredientWeights" type="number" step="0.01"></td>
+      <td><input data-target="formula.ingredientWeight" data-action="formula#calculateDoughWeight" type="number" step="0.01"></td>
       <td><button data-action="formula#deleteIngredient">-</button></td>
     `.trim()
   }
 
-  updateWeights() {
-    const totalFlourPercentage = 100; // Flour is always 100%
+  updateIngredientWeights() {
     const totalPercentage = this.ingredientPercentTargets.reduce((percent, element) => {
-      let elementValue = parseFloat(element.value)
-      if(!isNaN(elementValue))
-        return percent + elementValue
-    }, totalFlourPercentage)
+      const elementValue = this.sanitizeFloatInput(element.value)
+      return percent + elementValue
+    }, 0)
     this.totalPercentTarget.value = totalPercentage
 
-    const baseFlourWeight = this.baseFlourWeight
-    let totalWeight = baseFlourWeight
     this.ingredientWeightTargets.forEach((element, index) => {
-      let ingredientPercent = parseFloat(this.ingredientPercentTargets[index].value)
-      if(isNaN(ingredientPercent)) {
-        ingredientPercent = 0
-      }
-      let finalIngredientWeight = (ingredientPercent * this.baseFlourWeight) / 100
+      const ingredientPercent = this.sanitizeFloatInput(this.ingredientPercentTargets[index].value)
+      let finalIngredientWeight = (ingredientPercent * this.totalDoughWeight) / totalPercentage
       finalIngredientWeight = Math.round(finalIngredientWeight * 100) / 100.0
       element.value = finalIngredientWeight
-      totalWeight += finalIngredientWeight
     })
-    this.totalWeightTarget.value = totalWeight
   }
 
-  calculateFlourWeight(event) {
-    let newWeight = parseFloat(event.target.value)
-    if(isNaN(newWeight)) {
-      newWeight = 0
+  calculateDoughWeight(event) {
+    const newWeight = this.sanitizeFloatInput(event.target.value)
+    let totalPercent = parseInt(this.totalPercentTarget.value)
+    if(isNaN(totalPercent)) {
+      totalPercent = 0
     }
-    let ingredientPercent = parseFloat(event.target.parentElement.parentElement.cells[1].childNodes[0].value)
-    if(isNaN(ingredientPercent) || ingredientPercent <= 0) {
+
+    // If an ingredient has 0% then it doesn't affect the final dough weight, so we should quit early
+    const ingredientPercent = this.sanitizeFloatInput(event.target.parentElement.parentElement.cells[1].childNodes[0].value)
+    if(ingredientPercent <= 0) {
       return
     }
-    this.baseFlourWeight = (newWeight * 100.0) / ingredientPercent
+    this.totalDoughWeight = (newWeight * totalPercent) / ingredientPercent
   }
 
-  setFlourWeight(event) {
-    this.baseFlourWeight = event.target.value
-  }
-
-  get baseFlourWeight() {
-    const baseFlourWeight = this.flourWeightTarget.value
-    if(isNaN(baseFlourWeight)) {
-      return 500
+  sanitizeFloatInput(value) {
+    const floatValue = parseFloat(value)
+    if(isNaN(floatValue)) {
+      return 0
     }
-    return parseFloat(baseFlourWeight)
+    return floatValue
   }
 
-  set baseFlourWeight(value) {
+  get totalDoughWeight() {
+    return parseFloat(this.totalDoughWeightTarget.value)
+  }
+
+  set totalDoughWeight(value) {
     let floatValue = parseFloat(value)
     if(isNaN(floatValue)) {
-      this.flourWeightTarget.value = 500
+      this.totalDoughWeightTarget.value = 500
     }
     floatValue = Math.round(floatValue * 100) / 100.0
-    this.flourWeightTarget.value = floatValue
-    this.updateWeights()
+    this.totalDoughWeightTarget.value = floatValue
+    this.updateIngredientWeights()
   }
 
-  // TODO Allow editting other values to change baseFlourWeight
-  //      Generate data-key for building from a parameter
+  // TODO Generate data-key for building from a parameter
   //      Pull in data-key from URL when present, otherwise default recipe
   //      Stabilize table so it doesn't shrink when last ingredient is removed
   //      Choose "base" recipe and populate based on that
